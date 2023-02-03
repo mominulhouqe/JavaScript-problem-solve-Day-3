43c2a88b67a776b70699ddd33a8733ff317b50c5
Mominul Hoque <88490184+Momin69@users.noreply.github.com>
2023-02-03 23:12:23 +0600

    math

---
+++ math.js
@@ -0,0 +1,16 @@
+
+
+// finding gap absulate method
+const num1 = 32;
+const num2 = 43;
+const gap = Math.abs(num1 - num2);
+if(gap < 5){
+    console.log("You can be friend");
+}
+else{
+    console.log("Stay Appart");
+}
+
+// power math
+const result = Math.pow(2,4);
+console.log(result);
