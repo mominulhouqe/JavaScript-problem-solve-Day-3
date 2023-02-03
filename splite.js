55ba83d5d5de9e19bbdf61e798193d650c236a1e
Mominul Hoque <88490184+Momin69@users.noreply.github.com>
2023-02-03 21:36:33 +0600

    first commit

---
+++ splite.js
@@ -0,0 +1,20 @@
+
+
+const lyrics = "Amar sunar bangla. Ami Tumaye valobasi. Ciro din tumar akash. Tumar batas";
+
+const parts = lyrics.split('');
+// console.log(parts
+//     );
+const sentance = lyrics.split(' ');
+// console.log(sentance);
+const charts = lyrics.split(".");
+// console.log(charts);
+
+const mySong = [
+    'Amar sunar bangla',
+    ' Ami Tumaye valobasi',
+    ' Ciro din tumar akash',
+    ' Tumar batas'
+  ];
+  const ss = mySong.join( ". ")
+  console.log(ss);
